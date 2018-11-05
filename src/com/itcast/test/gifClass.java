4ad7130659bfee9ff84e1b407302e72628f18616
root <1104470017@qq.com>
2018-11-05 19:26:34 +0800

    我用idea提交了git

---
+++ src/com/itcast/test/gifClass.java
@@ -0,0 +1,16 @@
+package com.itcast.test;
+
+public class gifClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+        System.out.println("hello  idea?");
+    }
+}
